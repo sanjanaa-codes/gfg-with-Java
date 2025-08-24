a784b1f73d59501a6073c0c55fd76ebcb0dace65
R J Sanjanaa <sanjanaa0727@gmail.com>
2025-08-24 22:26:47 +0530

    Update Reverse Words.java

--- Reverse Words.java
+++ Reverse Words.java
@@ -11,3 +11,28 @@ class Solution {
 }
 
 _______________________________________________________________________________________________________
+    
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+        Stack<String> stack = new Stack<>();
+
+        // Split by one or more dots and push non-empty words onto the stack
+        for (String word : s.trim().split("\\.+")) {
+            if (!word.isEmpty()) {
+                stack.push(word);
+            }
+        }
+
+        // Pop from stack to reverse order and add dots between words
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+            if (!stack.isEmpty()) {
+                sb.append(".");
+            }
+        }
+
+        return sb.toString();
+    }
+}
+
